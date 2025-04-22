624d8cc2252bca1d503e49941b99b0d2774920b7
WilsonNguyen1211 <148605917+WilsonNguyen1211@users.noreply.github.com>
2025-04-22 19:10:30 -0400

    Feat: added mergeSort recursive function to split array ranges

--- MergeSort.cpp
+++ MergeSort.cpp
@@ -26,3 +26,12 @@ static void merge(vector<int>& arr, int left, int middle, int right) {
         arr[k++] = rightArr[j++];
     }
 }
+
+void mergeSort(vector<int>& arr, int left, int right) {
+    if (left < right) {
+        int mid = left + (right - left) / 2;
+        mergeSort(arr, left, mid);
+        mergeSort(arr, mid + 1, right);
+        merge(arr, left, mid, right);
+    }
+}
