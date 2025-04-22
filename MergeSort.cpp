c51c2baf27a8e5cafa4ed06157ccde2f2d369080
WilsonNguyen1211 <148605917+WilsonNguyen1211@users.noreply.github.com>
2025-04-22 19:05:31 -0400

    Feat: added merge function that takes two adjacent, sorted sub ranges of the array and puts them into one larger sorted range

---
+++ MergeSort.cpp
@@ -0,0 +1,28 @@
+#include <MergeSort.h>
+#include <vector>
+
+using namespace std;
+static void merge(vector<int>& arr, int left, int middle, int right) {
+    int n1 = middle - left + 1;
+    int n2 = right - middle;
+    vector<int> leftArr(n1);
+    vector<int> rightArr(n2);
+
+    for (int i = 0; i < n1; i++) {
+        leftArr[i] = arr[left + i];
+    }
+    for (int j = 0; j < n2; j++) {
+        rightArr[j] = arr[middle + 1 + j];
+    }
+
+    int i = 0, j = 0, k = left;
+    while (i < n1 && j < n2) {
+        arr[k++] = (leftArr[i] <= rightArr[j] ? leftArr[i++] : rightArr[j++]);
+    }
+    while (i < n1) {
+        arr[k++] = leftArr[i++];
+    }
+    while (j < n2) {
+        arr[k++] = rightArr[j++];
+    }
+}
